de5280f1b6869f70284018e6a132498e14cd2b8d
Angel Ivanov <angel.iwanov@gmail.com>
2024-08-07 23:06:51 +0300

    13.8

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,26 @@
 const router = require('express').Router();
 
 const { Blog } = require('../models');
+const { User } = require('../models');
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll();
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ['userID'] },
+    include: {
+      model: User,
+      attributes: ['name'],
+    },
+  });
   res.json(blogs);
 });
 
 router.post('/', async (req, res) => {
-  const blog = await Blog.create(req.body);
+  const user = await User.findOne();
+  const blog = await Blog.create({
+    ...req.body,
+    userId: user.id,
+    data: new Date(),
+  });
   res.json(blog);
 });
 
--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const middleware = require('./util/middleware');
 const { PORT } = require('./util/config');
 const { connectToDatabase } = require('./util/db');
 
+const userRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
 
 app.use(express.json());
 
+app.use('/api/users', userRouter);
 app.use('/api/blogs', blogsRouter);
+
 app.use(middleware.errorHandler);
 
 const start = async () => {

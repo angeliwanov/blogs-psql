27587349b5162bc14605a54564036809edf2d6b9
Angel Ivanov <angel.iwanov@gmail.com>
2024-08-10 20:56:44 +0300

    rollback

--- util/db.js
+++ util/db.js
@@ -2,20 +2,14 @@ const Sequelize = require('sequelize');
 const { DATABASE_URL } = require('./config');
 const { Umzug, SequelizeStorage } = require('umzug');
 
-const sequelize = new Sequelize(DATABASE_URL);
-
-const runMigrations = async () => {
-  const migrator = new Umzug({
-    migrations: { glob: './migrations/*.js' },
-    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  });
-  const migrations = await migrator.up();
-  console.log('Migrations up to date', {
-    files: migrations.map((mig) => mig.name),
-  });
-};
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 const connectToDatabase = async () => {
   try {
@@ -31,4 +25,26 @@ const connectToDatabase = async () => {
   return null;
 };
 
-module.exports = { connectToDatabase, sequelize };
+const migrationConf = {
+  migrations: {
+    glob: './migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf);
+  const migrations = await migrator.up();
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+const rollbackMigration = async () => {
+  await sequelize.authenticate();
+  const migrator = new Umzug(migrationConf);
+  await migrator.down();
+};
+
+module.exports = { connectToDatabase, sequelize, rollbackMigration };
